2acf76ee463d7cfbda8ac50596d20b96fae15d44
Dawid Janiga <dawidjaniga@gmail.com>
2025-03-16 01:32:34 +0100

    feat(K2): roam with testing ideas

--- packages/core/K2.ts
+++ packages/core/K2.ts
@@ -1,18 +1,17 @@
 import type { Channel } from "./types";
 import mitt, { type Emitter, type Handler } from "mitt";
-import { WebMidi, type Input, type Output, type InputChannel, type OutputChannel } from "webmidi";
+import { buttons } from "./controlls";
 
 type Events = {
     connect: void
     connectionError: unknown
+    'exit-setup.press': void
 }
 
 type EventNames = keyof Events
 
 export class K2 {
     private emitter: Emitter<Events>
-    private inputChannel: InputChannel
-    private outputChannel: OutputChannel
 
     constructor(private channel: Channel) {
         this.emitter = mitt()
@@ -20,25 +19,48 @@ export class K2 {
 
     async connect(): Promise<void> {
         try {
-            await WebMidi.enable()
-            const input = WebMidi.getInputByName('XONE:K2')
-            const output = WebMidi.getOutputByName('XONE:K2')
 
-            console.log({ input, output })
 
             if (input && output) {
-                this.inputChannel = input.channels[this.channel]
-                this.outputChannel = output.channels[this.channel]
-                this.emitter.emit('connect')
+
+
+                if (this.inputChannel && this.outputChannel) {
+                    this.emitter.emit('connect')
+
+                    this.attachEvents()
+                } else {
+                    console.log(`XONE:K2 Channel ${this.channel} not found`)
+                    this.emitter.emit('connectionError', new Error(`XONE:K2 Channel ${this.channel} not found`))
+                }
             } else {
-                this.emitter.emit('connectionError', new Error('XONE:K2 not found'))
+                console.log(`XONE:K2 not found on channel ${this.channel}`)
+                this.emitter.emit('connectionError', new Error(`XONE:K2 not found on channel ${this.channel}`))
             }
         } catch (error) {
+            console.log('Error connecting to K2', error)
             this.emitter.emit('connectionError', error)
         }
     }
 
     on<T extends EventNames>(event: T, callback: Handler<Events[T]>): void {
         this.emitter.on(event, callback)
+    }
+
+    attachEvents() {
+        console.log('attaching events')
+
+        if (this.inputChannel) {
+            this.inputChannel.addListener('noteon', (e) => {
+                console.log('noteon', e)
+
+                const button = buttons.find(b => b.midi === e.note.number);
+
+                if (button) {
+                    const eventName = button.name + '.press' as keyof Events
+
+                    this.emitter.emit(eventName, button);
+                }
+            })
+        }
     }
 }   
--- packages/core/k2.spec.ts
+++ packages/core/k2.spec.ts
@@ -1,17 +1,29 @@
-import { describe, expect, it, beforeEach, vi } from "vitest"
+import { describe, expect, it, beforeEach, vi, beforeAll } from "vitest"
 import * as WMT from 'web-midi-test';
 
 import { K2 } from "./K2"
 
-// navigator.requestMIDIAccess = WMT.requestMIDIAccess;
+class MIDIMessageEvent extends Event {
+    constructor(type: string, eventInitDict: { data: Uint8Array }) {
+        super(type);
+        this.data = eventInitDict.data;
+    }
+    data: Uint8Array;
+}
 
+let midiAccess: MIDIAccess;
+let controller: K2;
+let input: MIDIInput;
+let output: MIDIOutput;
 
 describe("K2", () => {
-    beforeEach(() => {
+    it('should connect to the device on the given channel', async () => {
+        const midiAccess = await navigator.requestMIDIAccess();
 
-    })
+        // Add extra devices
+        midiAccess.addInput("input-2", "XONE:K2");
+        midiAccess.addOutput("output-2", "XONE:K2");
 
-    it.only('should connect to the device on the given channel', async () => {
         const channel = 2
         const k2 = new K2(channel)
 
@@ -23,74 +35,64 @@ describe("K2", () => {
         expect(handler).toHaveBeenCalled()
     })
 
-    it.skip('should throw an error if the WebMIDI connection is insecure', async () => {
+    it.skip('notify about button press', async () => {
         const k2 = new K2(2)
 
-        // prepare WMT to throw an error
-        // WMT.requestMIDIAccess.mockRejectedValue(new Error('Insecure connection'))
-
         const handler = vi.fn()
-        k2.on('connectionError', handler)
+        k2.on('exit-setup.press', handler)
 
         await k2.connect()
+        k2.attachEvents()
+
+
+        // const input = Array.from(inputs.values())[0]
+        // input.dispatchEvent(new MIDIMessageEvent('midimessage', {
+        //     data: new Uint8Array([144, 23, 127])
+        // }))
 
         expect(handler).toHaveBeenCalled()
     })
 
-    it('should notify about insecure connection', () => {
-        const k2 = new K2(2)
-        // k2.on('insecureConnection', () => {
-        //     expect(true).toBe(true)
-        // })
-    })
-
-    it('notify about button press', () => {
-        // const k2 = new K2(2)
-        // k2.on('buttonPress', (button) => {
-        //     expect(button).toBe(1)
-        // })
-    })
-
-    it('notify about button release', () => {
+    it.skip('notify about button release', () => {
         // const k2 = new K2(2)
         // k2.on('buttonRelease', (button) => {
         //     expect(button).toBe(1)
         // })
     })
 
-    it('should return all buttons', () => {
+    it.skip('should return all buttons', () => {
         // const k2 = new K2(2)
         // const buttons = k2.getButtons()
         // expect(buttons).toBeDefined()
     })
 
-    it('should notify about fader change', () => {
+    it.skip('should notify about fader change', () => {
         // const k2 = new K2(2)
         // k2.on('faderChange', (fader) => {
         //     expect(fader).toBe(1)
         // })
     })
 
-    it('should notify about knob change', () => {
+    it.skip('should notify about knob change', () => {
         // const k2 = new K2(2)
         // k2.on('knobChange', (knob) => {
         //     expect(knob).toBe(1)
         // })
     })
 
-    it('should highlight the given button', () => {
+    it.skip('should highlight the given button', () => {
         // const k2 = new K2(2)
         // k2.highlightButton(1)
         // expect(k2.isButtonHighlighted(1)).toBe(true)
     })
 
-    it('should unhighlight the given button', () => {
+    it.skip('should unhighlight the given button', () => {
         // const k2 = new K2(2)
         // k2.unhighlightButton(1)
         // expect(k2.isButtonHighlighted(1)).toBe(false)
     })
 
-    it('should unhighlight all buttons', () => {
+    it.skip('should unhighlight all buttons', () => {
         // const k2 = new K2(2)
         // k2.unhighlightAllButtons()
         // expect(k2.isButtonHighlighted(1)).toBe(false)
