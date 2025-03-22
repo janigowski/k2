8c20fdc036aab2f7f15af29977255af84f3b19dd
Dawid Janiga <dawidjaniga@gmail.com>
2025-03-22 17:30:01 +0100

    feat(BrowserMIDI): satisfy interface for MIDIInput

--- packages/core/BrowserMIDIProvider/BrowserMIDIProvider.ts
+++ packages/core/BrowserMIDIProvider/BrowserMIDIProvider.ts
@@ -1,46 +1,64 @@
 import type { MIDIEventName, MIDIEventTypes, MIDIIdentifierOptions, MIDIInput, MIDIOutput, MIDIProvider } from "../interfaces/MIDIProvider";
+import { BrowserMIDIInput } from "./BrowserMIDIInput";
 
 export class BrowserMIDIProvider implements MIDIProvider {
-    private midiAccess: WebMidi.MIDIAccess | null = null
+    private midiAccess!: WebMidi.MIDIAccess
     private inputs: Record<string, MIDIInput> = {}
     private outputs: Record<string, MIDIOutput> = {}
 
     constructor() {
-
     }
 
     async connect() {
         this.midiAccess = await navigator.requestMIDIAccess()
-        const input = Array.from(midiAccess.inputs.values()).find(item => item.name === 'XONE:K2')
-        const output = Array.from(midiAccess.outputs.values()).find(item => item.name === 'XONE:K2')
+
+        if (!this.midiAccess) {
+            throw new Error('MIDI access not granted')
+        }
     }
 
     async disconnect() {
     }
 
     getInput(options: MIDIIdentifierOptions): MIDIInput | undefined {
-        const key = this.getInputKey(options)
+        const key = this.getPortKey(options)
+
+        if (!this.inputs[key]) {
+            const input = Array.from(this.midiAccess.inputs.values()).find(item => item.name === options.name)
+
+            if (input) {
+                this.setInput(options, new BrowserMIDIInput(options.name || 'Unknown', options.channel, input))
+            }
+        }
 
         return this.inputs[key]
     }
 
     getOutput(options: MIDIIdentifierOptions): MIDIOutput | undefined {
-        const key = this.getInputKey(options)
+        const key = this.getPortKey(options)
+
+        // if (!this.outputs[key]) {
+        //     const output = Array.from(this.midiAccess.outputs.values()).find(item => item.name === options.name)
+
+        //     if (output) {
+        //         this.setOutput(options, new BrowserMIDIOutput(options.name || 'Unknown', output))
+        //     }
+        // }
 
         return this.outputs[key]
     }
 
     setInput(options: MIDIIdentifierOptions, input: MIDIInput) {
-        const key = this.getInputKey(options)
+        const key = this.getPortKey(options)
         this.inputs[key] = input
     }
 
     setOutput(options: MIDIIdentifierOptions, output: MIDIOutput) {
-        const key = this.getInputKey(options)
+        const key = this.getPortKey(options)
         this.outputs[key] = output
     }
 
-    private getInputKey(options: MIDIIdentifierOptions): string {
+    private getPortKey(options: MIDIIdentifierOptions): string {
         return `${options.name}-${options.channel}`
     }
 
