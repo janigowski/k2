32257e30c466aacd97ccc72a7b4715203d1de162
Dawid Janiga <dawidjaniga@gmail.com>
2025-03-24 17:12:48 +0100

    feat(examples): init Svelte

---
+++ packages/examples/svelte/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>K2 Svelte Example</h1>
+</main>
+
+<style>
+  main {
+    padding: 2rem;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 2em;
+    margin: 0;
+  }
+</style>
