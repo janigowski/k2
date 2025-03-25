2c5c83338026918a9ab54f4bde8319039fe86ec9
Dawid Janiga <dawidjaniga@gmail.com>
2025-03-25 13:54:18 +0100

    test: collect coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,13 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
         setupFiles: "./packages/core/BrowserMIDIProvider/test/setup.ts",
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'json', 'html'],
+            include: ['packages/**/*.{ts,tsx}'],
+            exclude: ['**/*.d.ts', '**/*.test.{ts,tsx}', '**/node_modules/**'],
+            reportsDirectory: './coverage',
+            all: true
+        }
     },
 });
